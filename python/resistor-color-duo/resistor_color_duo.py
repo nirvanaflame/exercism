b6eeee03bd6d8b9adc13e7be35f80a6038dc194f
nf <sergey.putik@mail.ru>
2023-04-16 00:02:53 +0200

    (fix): resistor_color_duo.py #python

--- python/resistor-color-duo/resistor_color_duo.py
+++ python/resistor-color-duo/resistor_color_duo.py
@@ -1,13 +1,5 @@
 col = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
 
-def value(colors):
-    first = col.index(colors[0])
-    second = str(col.index(colors[1]))
-    print(second)
-    return int(str(first) if first != 0 else "" + second)
 
-
-def get_first(n: int) -> str:
-    if n == 0:
-        return ''
-    return str(n)
+def value(colors: list):
+    return col.index(colors[0]) * 10 + col.index(colors[1])
