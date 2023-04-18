3794b62398c5a5236b935f24fd704c34940d69c6
nf <sergey.putik@mail.ru>
2023-04-18 23:24:37 +0200

    robot #java #stream

---
+++ java/robot-name/src/main/java/Robot.java
@@ -0,0 +1,49 @@
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+import java.util.stream.IntStream;
+
+public class Robot {
+
+    public String name;
+
+    public static final Set<String> mem = new HashSet<>();
+    private static final Random random = new Random();
+
+    Robot() {
+        reset();
+    }
+
+    public void reset() {
+        this.name = generateUniqueName();
+    }
+
+
+    private static String generateUniqueName() {
+        String name = generateName();
+        if (mem.contains(name)) {
+            generateUniqueName();
+        }
+
+        mem.add(name);
+        return name;
+    }
+
+    private static String generateName() {
+        return IntStream
+                .concat(
+                        IntStream.generate(() -> random.nextInt('A', 'Z' + 1)).limit(2),
+                        IntStream.generate(() -> random.nextInt('0', '9' + 1)).limit(3)
+                )
+                .collect(
+                        StringBuilder::new,
+                        StringBuilder::appendCodePoint,
+                        StringBuilder::append
+                )
+                .toString();
+    }
+
+    public String getName() {
+        return name;
+    }
+}
