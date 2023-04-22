f40cf24bfbdf889f9a299aa015d653c6905c23b2
nf <sergey.putik@mail.ru>
2023-04-22 23:48:43 +0200

    tuples.py #python

---
+++ python/tisbury-treasure-hunt/tuples.py
@@ -0,0 +1,66 @@
+"""Functions to help Azara and Rui locate pirate treasure."""
+
+
+def get_coordinate(record):
+    """Return coordinate value from a tuple containing the treasure name, and treasure coordinate.
+
+    :param record: tuple - with a (treasure, coordinate) pair.
+    :return: str - the extracted map coordinate.
+    """
+    return record[1]
+
+
+def convert_coordinate(coordinate):
+    """Split the given coordinate into tuple containing its individual components.
+
+    :param coordinate: str - a string map coordinate
+    :return: tuple - the string coordinate split into its individual components.
+    """
+
+    return coordinate[0], coordinate[1]
+
+
+def compare_records(azara_record, rui_record):
+    """Compare two record types and determine if their coordinates match.
+
+    :param azara_record: tuple - a (treasure, coordinate) pair.
+    :param rui_record: tuple - a (location, tuple(coordinate_1, coordinate_2), quadrant) trio.
+    :return: bool - do the coordinates match?
+    """
+    return str(azara_record[1]) == ''.join(rui_record[1])
+
+
+def create_record(azara_record, rui_record):
+    """Combine the two record types (if possible) and create a combined record group.
+
+    :param azara_record: tuple - a (treasure, coordinate) pair.
+    :param rui_record: tuple - a (location, coordinate, quadrant) trio.
+    :return: tuple or str - the combined record (if compatible), or the string "not a match" (if incompatible).
+    """
+
+    return azara_record + rui_record if compare_records(azara_record, rui_record) else 'not a match'
+
+
+def clean_up(combined_record_group):
+    """Clean up a combined record group into a multi-line string of single records.
+
+    :param combined_record_group: tuple - everything from both participants.
+    :return: str - everything "cleaned", excess coordinates and information are removed.
+
+    The return statement should be a multi-lined string with items separated by newlines.
+
+    (see HINTS.md for an example).
+    """
+    sb = ""
+    for tup in combined_record_group:
+        sb += '('
+        for i, x in enumerate(tup):
+            if i != 1:
+                if isinstance(x, tuple):
+                    sb += str(x)
+                else:
+                    sb += f"'{x}'"
+            if i not in (1, len(tup) - 1):
+                sb += ', '
+        sb += ')\n'
+    return sb
