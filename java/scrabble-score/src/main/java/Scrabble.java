f0f0228a51de7a13eaa9b53db67cc606dcf2c854
nf <sergey.putik@mail.ru>
2023-04-23 18:25:51 +0200

    scrabble-score #java

---
+++ java/scrabble-score/src/main/java/Scrabble.java
@@ -0,0 +1,54 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+class Scrabble {
+
+    private String word;
+
+    Scrabble(String word) {
+        this.word = word;
+    }
+
+    int getScore() {
+        return word.isBlank()
+                ? 0
+                : Arrays.stream(word.split(""))
+                .mapToInt(this::getLetterScore)
+                .sum();
+    }
+
+    private static final HashMap<String, Integer> letterScores = new HashMap<>() {
+        {
+            put("A", 1);
+            put("E", 1);
+            put("I", 1);
+            put("O", 1);
+            put("U", 1);
+            put("L", 1);
+            put("N", 1);
+            put("R", 1);
+            put("S", 1);
+            put("T", 1);
+            put("D", 2);
+            put("G", 2);
+            put("B", 3);
+            put("C", 3);
+            put("M", 3);
+            put("P", 3);
+            put("F", 4);
+            put("H", 4);
+            put("V", 4);
+            put("W", 4);
+            put("Y", 4);
+            put("K", 5);
+            put("J", 8);
+            put("X", 8);
+            put("Q", 10);
+            put("Z", 10);
+        }
+    };
+
+    private int getLetterScore(String ch) {
+        return letterScores.get(ch.toUpperCase());
+    }
+}
--- python/binary-search/binary_search.py
+++ python/binary-search/binary_search.py
@@ -3,7 +3,7 @@ def find(search_list, value):
     right = len(search_list) - 1
 
     while left <= right:
-        mid = int((left + right)/2)
+        mid = left + (right - left) // 2
         if search_list[mid] == value:
             return mid
         elif search_list[mid] < value:
