3c718f2a0a673a98e8c0499cbb7076bd89399bb9
nf <sergey.putik@mail.ru>
2023-04-23 15:52:52 +0200

    anagram.py #python #refactoring

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -2,21 +2,9 @@ import string
 
 
 def find_anagrams(word, candidates):
-    candidates = list(filter(lambda x: word.lower() != x.lower(), candidates))
-    word = encode_anagram(word)
-    return [candidate for candidate in candidates if is_anagram(word, encode_anagram(candidate))]
-
-
-def encode_anagram(word):
-    if " " in word:
-        return "-1"
-
-    mem = [0] * len(string.ascii_lowercase)
-    for ch in word.lower():
-        index = string.ascii_lowercase.index(ch)
-        mem[index] += 1
-    return str(mem)
-
-
-def is_anagram(word, candidate):
-    return word == candidate
+    return [
+        x
+        for x in candidates
+        if word.casefold() != x.casefold()
+        and sorted(word.casefold()) == sorted(x.casefold())
+    ]
