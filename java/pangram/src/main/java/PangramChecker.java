ea7cfa758cb9e30fb5921fac9378502d7deaf367
nf <sergey.putik@mail.ru>
2023-04-25 22:29:11 +0200

    PangramChecker.java #java

---
+++ java/pangram/src/main/java/PangramChecker.java
@@ -0,0 +1,11 @@
+public class PangramChecker {
+
+    public boolean isPangram(String input) {
+        return input
+                .toLowerCase()
+                .chars()
+                .filter(Character::isLetter)
+                .distinct()
+                .count() == 26;
+    }
+}
