11dea74e7974d2953ed1dcd761a9aaaeb1fc152e
nf <sergey.putik@mail.ru>
2023-04-25 22:06:18 +0200

    HighScores.java #java

---
+++ java/high-scores/src/main/java/HighScores.java
@@ -0,0 +1,28 @@
+import java.util.Comparator;
+import java.util.List;
+
+class HighScores {
+
+    List<Integer> highScores;
+
+    public HighScores(List<Integer> highScores) {
+        this.highScores = highScores;
+    }
+
+    List<Integer> scores() {
+        return highScores;
+    }
+
+    Integer latest() {
+        return highScores.get(highScores.size() - 1);
+    }
+
+    Integer personalBest() {
+        return highScores.stream().max(Integer::compareTo).orElse(0);
+    }
+
+    List<Integer> personalTopThree() {
+        return highScores.stream().sorted(Comparator.reverseOrder()).limit(3).toList();
+    }
+
+}
