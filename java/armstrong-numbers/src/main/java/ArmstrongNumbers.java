eb20d7ddd651db3528f9eacb613b23ff57a8386b
nf <sergey.putik@mail.ru>
2023-04-26 21:13:20 +0200

    ArmstrongNumbers.java #java

---
+++ java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -0,0 +1,10 @@
+class ArmstrongNumbers {
+
+    boolean isArmstrongNumber(int numberToCheck) {
+        String num = String.valueOf(numberToCheck);
+        int len = num.length();
+        int poweredDigs = num.chars().map(x -> (int) Math.pow(Character.digit(x, 10), len)).sum();
+        return numberToCheck == poweredDigs;
+    }
+
+}
