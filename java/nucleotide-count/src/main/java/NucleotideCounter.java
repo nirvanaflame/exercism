314d8aac568d013ab76b1fdd6a1540129995c09a
nf <sergey.putik@mail.ru>
2023-05-09 22:28:29 +0200

    NucleotideCounter.java #java

--- java/nucleotide-count/src/main/java/NucleotideCounter.java
+++ java/nucleotide-count/src/main/java/NucleotideCounter.java
@@ -1,28 +1,26 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
 
 public class NucleotideCounter {
 
-    private String nucleotide;
+    private final Map<Character, Integer> counts = new HashMap<>() {{
+        Stream.of('A', 'C', 'G', 'T').forEach(ch -> put(ch, 0));
+    }};
 
-    public NucleotideCounter(String nucleotide) {
-        if (nucleotide.matches("[ATGC]*")) {
-            this.nucleotide = nucleotide;
-        } else {
-            throw new IllegalArgumentException();
-        }
+    public NucleotideCounter(String strand) {
+        Consumer<Character> isValid = ch -> {
+            if (!counts.containsKey(ch)) throw new IllegalArgumentException();
+        };
+
+        strand.chars()
+                .mapToObj(code -> (char) code)
+                .peek(isValid)
+                .forEach(ch -> counts.merge(ch, 1, Integer::sum));
     }
 
     public Map<Character, Integer> nucleotideCounts() {
-        var map = new HashMap<Character, Integer>() {{
-            put('A', 0);
-            put('C', 0);
-            put('G', 0);
-            put('T', 0);
-        }};
-        nucleotide.chars()
-                .mapToObj(code -> (char) code)
-                .forEach(x -> map.merge(x, 1, Integer::sum));
-        return map;
+        return counts;
     }
 }
