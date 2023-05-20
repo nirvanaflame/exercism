b4269e126a648aaa863529a67616e9f2f0d70d3f
nf <sergey.putik@mail.ru>
2023-05-20 11:28:43 +0200

    sum_of_multiples.py #python

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,2 +1,8 @@
-def sum_of_multiples(limit, multiples):
-    pass
+def sum_of_multiples(level: int, multiples: []):
+    mem = set()
+    for multiply in multiples:
+        base = multiply
+        while base != 0 and base < level:
+            mem.add(base)
+            base += multiply
+    return sum(mem)
