33042c16b164ba6a6e1c7c0241536186030ddd55
nf <s.putik@bk.ru>
2026-09-21 21:12:50 +0200

    Attack of trolls

---
+++ csharp/attack-of-the-trolls/AttackOfTheTrolls.cs
@@ -0,0 +1,42 @@
+enum AccountType
+{
+    Guest,
+    User,
+    Moderator
+}
+
+[Flags]
+enum Permission
+{
+    None = 0,
+    Read = 1,
+    Write = 2,
+    Delete = 4,
+    All = Read | Write | Delete
+}
+
+static class Permissions
+{
+    public static Permission Default(AccountType accountType) => accountType switch
+    {
+        AccountType.Guest => Permission.Read,
+        AccountType.User => Permission.Read | Permission.Write,
+        AccountType.Moderator => Permission.All,
+        _ => Permission.None
+    };
+
+    public static Permission Grant(Permission current, Permission grant)
+    {
+        return current | grant;
+    }
+
+    public static Permission Revoke(Permission current, Permission revoke)
+    {
+        return current & ~revoke;
+    }
+
+    public static bool Check(Permission current, Permission check)
+    {
+        return (current & check) == check;
+    }
+}
